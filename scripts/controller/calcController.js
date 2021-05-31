b3a5fe30f9d0624d62323acb898f30cdb20157bf
Cristian Camargo <chris.camargo2015@gmail.com>
2021-05-31 19:33:28 -0400

    aula-2-encapsulamento-get-and-set

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -6,4 +6,29 @@ class CalcController {
     // dentro da classe sempre encontramos variáveis e funções
 
 
+    constructor() {
+
+        //dentro dos parenteses passamos os parametros, e dentro das chaves o que vai fazer
+
+        this._displayCalc = "0";
+        this._dataAtual;
+
+        // o comando interno this referencia  atributos e métodos.
+    }
+
+    get displayCalc() {
+        return this._displayCalc;
+    }
+    set displayCalc(valor) {
+        this._displayCalc = valor;
+    }
+
+    get dataAtual() {
+        return this._dataAtual;
+    }
+
+    set dataAtual(valor) {
+        this.dataAtual = valor;
+    }
+
 }
