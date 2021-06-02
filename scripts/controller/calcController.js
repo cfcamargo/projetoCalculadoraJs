773b5580d8021bafdc25e4dd897bc4f5efc33a93
Cristian Camargo <chris.camargo2015@gmail.com>
2021-06-01 21:20:41 -0400

    switch-case

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -8,7 +8,8 @@ class CalcController {
     constructor() {
 
         //dentro dos parenteses passamos os parametros, e dentro das chaves o que vai fazer
-        this._locale = 'pt-br'
+        this._operation = [];
+        this._locale = 'pt-br';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
@@ -32,9 +33,88 @@ class CalcController {
 
     addEventListenerAll(element, events, fn) {
 
+        //criando uma forma de selecionar vaior eventos usando um separador de array(split)
+
         events.split(' ').forEach(event => {
             element.addEventListener(event, fn, false);
         });
+
+    }
+
+    clearAll() {
+        this._operation = [];
+    }
+
+
+    clearEntry() {
+        this._operation.pop();
+    }
+
+    addOperation(value) {
+        this._operation.push(value);
+        console.log(this._operation);
+    }
+
+    setError() {
+        this.displayCalc = 'Error';
+    }
+
+    execBtn(value) {
+
+        switch (value) {
+
+            case 'ac':
+                this.clearAll();
+                break;
+
+            case 'ce':
+                this.clearEntry();
+                break;
+
+            case 'soma':
+
+                break;
+
+            case 'subtracao':
+
+                break;
+
+            case 'divisao':
+
+                break;
+
+            case 'multiplicacao':
+
+                break;
+
+            case 'porcento':
+
+                break;
+
+            case 'igual':
+
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+
+            default:
+                this.setError();
+                break;
+
+        }
+
 
     }
 
@@ -45,7 +125,10 @@ class CalcController {
         buttons.forEach((btn, index) => {
 
             this.addEventListenerAll(btn, "click drag", e => {
-                console.log(btn.className.baseVal.replace("btn-", ""));
+
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn(textBtn);
             });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
