92f495a0d696532e13e4b8c727393475e9cad768
Cristian Camargo <chris.camargo2015@gmail.com>
2021-06-07 19:24:18 -0400

    eval-validando-primeiro-operador-calculando-resultado

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -65,9 +65,32 @@ class CalcController {
 
     }
 
+    pushOperator(value) {
+        this._operation.push(value);
+
+        if (this._operation.length > 3) {
+
+            this.calc();
+
+        }
+    }
+
+    calc() {
+
+
+        let last = this._operation.pop();
+
+        let result = eval(this._operation.join(""));
+
+        this._operation = [result, last];
+
+
+
+    }
+
+
     addOperation(value) {
 
-        console.log('A', isNaN(this.getLastOperation()));
 
         if (isNaN(this.getLastOperation())) {
 
@@ -77,23 +100,33 @@ class CalcController {
 
             } else if (isNaN(value)) {
 
-                console.log(value);
+
+                console.log('outra coisa', value);
 
             } else {
 
-                this._operation.push(value);
+                this.pushOperator(value);
 
             }
 
         } else {
 
-            let newValue = this.getLastOperation().toString() + value.toString();
+            if (this.isOperator(value)) {
 
-            this.setLastOperation(parseInt(newValue));
+                this.pushOperator(value);
+
+            } else {
+
+                let newValue = this.getLastOperation().toString() + value.toString();
+
+                this.setLastOperation(parseInt(newValue));
+
+            }
+
+
 
         }
 
-        console.log(this._operation);
 
     }
 
